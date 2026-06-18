3274ac97758f4cd18f04e7f76146a25653abddd8
Danil <danilbarabash@icloud.com>
2026-06-18 12:58:50 +0200

    chore(project): add hilt for dependency injecktion chore(app): configure Hilt plugin and dependencies in app module chore(domain): add javax.inject to domain module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -53,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
