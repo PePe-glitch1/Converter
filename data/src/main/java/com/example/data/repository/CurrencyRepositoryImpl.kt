fb15106749c6305feb51736eee15fbae13aba94f
Danil <danilbarabash@icloud.com>
2026-07-09 22:16:11 +0200

    feat(data/repository): add getAllCurrencies and getAllFromApi to CurrencyRepository

--- data/src/main/java/com/example/data/repository/CurrencyRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/CurrencyRepositoryImpl.kt
@@ -2,15 +2,26 @@ package com.example.data.repository
 
 import com.example.data.local.dao.CurrencyDao
 import com.example.data.local.mapper.toCurrency
+import com.example.data.local.mapper.toCurrencyEntity
+import com.example.data.remote.ApiService
 import com.example.domin.model.Currency
 import com.example.domin.repository.CurrencyRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.first
 
 class CurrencyRepositoryImpl @Inject constructor(
-    private val currencyDao: CurrencyDao
+    private val currencyDao: CurrencyDao,
+    private val currencyApi: ApiService,
 ) : CurrencyRepository {
 
     override suspend fun getCurrencyByCode(code: String): Currency =
         currencyDao.getCurrencyByCode(code).first().toCurrency()
+
+    override suspend fun getAllCurrencies(): List<Currency> =
+        currencyDao.getAllCurrencies().map { it.toCurrency() }
+
+    override suspend fun getAllFromApi() {
+        currencyDao.upsertCurrency(currencyApi.getAllCurrencies().toCurrencyEntity())
+    }
+
 }
--- domain/src/main/java/com/example/domin/repository/CurrencyRepository.kt
+++ domain/src/main/java/com/example/domin/repository/CurrencyRepository.kt
@@ -4,4 +4,6 @@ import com.example.domin.model.Currency
 
 interface CurrencyRepository{
     suspend fun getCurrencyByCode(code: String): Currency
+    suspend fun getAllCurrencies(): List<Currency>
+    suspend fun getAllFromApi()
 }
