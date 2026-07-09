ff1422b33ebe6f0c8c9f9e7bd52cb2d19daad181
Danil <danilbarabash@icloud.com>
2026-07-09 22:15:59 +0200

    feat(data/di): add Retrofit and OkHttp providers

--- data/src/main/java/com/example/data/local/di/DatabaseModule.kt
+++ data/src/main/java/com/example/data/local/di/DatabaseModule.kt
@@ -5,26 +5,50 @@ import androidx.room.Room
 import com.example.data.local.dao.CurrencyDao
 import com.example.data.local.dao.HistoryDao
 import com.example.data.local.database.AppDatabase
+import com.example.data.remote.ApiService
+import com.example.data.repository.CurrencyRepositoryImpl
+import com.example.domin.repository.CurrencyRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import jakarta.inject.Singleton
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
-    @Singleton
-    @Provides
+
+    @Singleton @Provides
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+            .build()
+
+    @Singleton @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Singleton @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+
+    @Singleton @Provides
     fun provideDatabase(@ApplicationContext ctx: Context): AppDatabase =
         Room.databaseBuilder(ctx, AppDatabase::class.java, "app_database").build()
 
-    @Singleton
-    @Provides
+    @Singleton @Provides
     fun provideCurrencyDao(database: AppDatabase): CurrencyDao = database.currencyDao()
 
-    @Singleton
-    @Provides
+    @Singleton @Provides
     fun provideHistoryDao(database: AppDatabase): HistoryDao = database.historyDao()
+
 }
