f69e62ff710771acac75313aacd3bb4c1921f813
Danil <danilbarabash@icloud.com>
2026-07-09 22:16:07 +0200

    refactor(data/mapper): add CurrencyDto to CurrencyEntity mapping

--- data/src/main/java/com/example/data/local/mapper/CurrencyMapper.kt
+++ data/src/main/java/com/example/data/local/mapper/CurrencyMapper.kt
@@ -1,6 +1,7 @@
 package com.example.data.local.mapper
 
 import com.example.data.local.entity.CurrencyEntity
+import com.example.data.remote.dto.CurrencyDto
 import com.example.domin.model.Currency
 
 fun CurrencyEntity.toCurrency() = Currency(
@@ -9,8 +10,12 @@ fun CurrencyEntity.toCurrency() = Currency(
     data = data,
 )
 
-fun Currency.toCurrencyEntity() = CurrencyEntity(
-    code = code,
-    perToDollar = perToDollar,
-    data = data,
-)
+fun CurrencyDto.toCurrencyEntity() : List<CurrencyEntity> {
+    return usd.map { (code, value) ->
+        CurrencyEntity(
+            code = code,
+            perToDollar = value,
+            data = date
+        )
+    }
+}
