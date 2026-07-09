4cb1232b03c20ff5ad48d2947f5143943e7f0353
Danil <danilbarabash@icloud.com>
2026-07-09 22:16:03 +0200

    feat(data/dao): add getAllCurrencies and upsertCurrency to CurrencyDao

--- data/src/main/java/com/example/data/local/dao/CurrencyDao.kt
+++ data/src/main/java/com/example/data/local/dao/CurrencyDao.kt
@@ -2,6 +2,7 @@ package com.example.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Upsert
 import com.example.data.local.entity.CurrencyEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -10,5 +11,11 @@ interface CurrencyDao {
 
      @Query("SELECT * FROM currency Where code = :code")
      fun getCurrencyByCode(code: String): Flow<CurrencyEntity>
+
+     @Query("SELECT * FROM currency")
+     suspend fun getAllCurrencies(): List<CurrencyEntity>
+
+     @Upsert
+     suspend fun upsertCurrency(currency: List<CurrencyEntity>)
 
 }
