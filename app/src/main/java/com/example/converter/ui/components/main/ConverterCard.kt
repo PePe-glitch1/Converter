28a4e76e00b7dfb1041c2efbb490c015aa63c2a0
Danil <danilbarabash@icloud.com>
2026-07-21 21:09:58 +0200

    feat(ui/components): add ConverterCard composable skeleton

---
+++ app/src/main/java/com/example/converter/ui/components/main/ConverterCard.kt
@@ -0,0 +1,19 @@
+package com.example.converter.ui.components.main
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.converter.ui.theme.ConverterTheme
+
+@Composable
+fun ConvertorCard(){
+    Column() { }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowConvertorCard(){
+    ConverterTheme {
+        ConvertorCard()
+    }
+}
