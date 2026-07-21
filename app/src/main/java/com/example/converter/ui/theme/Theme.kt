ca22f89350167248743b8e6985de6e27294ef05b
Danil <danilbarabash@icloud.com>
2026-07-21 20:50:42 +0200

    stile(ui/theme): add colot in theme for tests

--- app/src/main/java/com/example/converter/ui/theme/Theme.kt
+++ app/src/main/java/com/example/converter/ui/theme/Theme.kt
@@ -18,6 +18,7 @@ private val DarkColorScheme = darkColorScheme(
     onBackground = White,
     surface = InputBackground,
     onSurface = White,
+    surfaceVariant = InputBackground, //background for tests
     onSurfaceVariant = TextMuted,
 )
 
@@ -29,6 +30,7 @@ private val LightColorScheme = lightColorScheme(
     onBackground = White,
     surface = InputBackground,
     onSurface = White,
+    surfaceVariant = InputBackground, //background for tests
     onSurfaceVariant = TextMuted,
 )
 
@@ -36,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 fun ConverterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
